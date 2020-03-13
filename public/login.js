071721c66592782c00fd514ee204f0baea155173
Kushagrav7 <Kushagrav7@outlook.com>
2020-03-12 23:06:39 -0700

    refresh

--- public/login.js
+++ public/login.js
@@ -27,8 +27,8 @@ function login(){
 
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
-
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) 
+  {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
@@ -36,10 +36,16 @@ function login(){
     window.alert("Error : " + errorMessage);
 
     // ...
-  });
-
+  }); 
+  if (document.referrer !== document.location.href) {
+    setTimeout(function() {
+        document.location.reload()
+  }, 400);
 }
+}
+
 
 function logout(){
   firebase.auth().signOut();
+  location.reload();
 }
